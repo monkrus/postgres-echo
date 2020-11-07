36565617925da820b01a6533c6f51d84be002b22
Sergei Stadnik <sergeisqa@gmail.com>
2020-11-07 08:09:30 -0700

    student.go corrected

--- model/student.go
+++ model/student.go
@@ -1,26 +1,6 @@
-package controller
+package model
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-	"github.com/oxlb/GoLangPostgresHelloWorld/model"
-	"github.com/oxlb/GoLangPostgresHelloWorld/storage"
-)
-
-// GetStudents
-func GetStudents(c echo.Context) error {
-	students, _ := GetRepoStudents()
-	return c.JSON(http.StatusOK, students)
-}
-
-func GetRepoStudents() ([]model.Students, error) {
-	db := storage.GetDBInstance()
-	students := []model.Students{}
-
-	if err := db.Find(&students).Error; err != nil {
-		return nil, err
-	}
-
-	return students, nil
+type Students struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }
